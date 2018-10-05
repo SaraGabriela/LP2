f639f9a987cfab77e227ec9182ee6fbc5719fbea
Unknown <sara.diaz.oporto@gmail.com>
2018-10-04 22:40:19 -0500

    Figures in QT

---
+++ figures/mainwindow.cpp
@@ -0,0 +1,76 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+	QMainWindow(parent),
+	ui(new Ui::MainWindow)
+{
+	ui->setupUi(this);
+
+	pixmap = new QPixmap(600, 400);
+	pixmap->fill();
+
+	pen = new QPen(QColor("black"));
+
+	ui->label_draw_area->setPixmap(*pixmap);
+
+	painter = new QPainter(pixmap);
+	painter->setPen(*pen);
+}
+
+MainWindow::~MainWindow()
+{
+	delete ui;
+    for(Figure * s:shapes)
+        delete s;
+}
+
+void MainWindow::draw_circle(QPainter * painter)
+{
+    shapes.push_back(new Circle("red",rand() % 100,rand() % 500,rand() % 400));
+    for(Figure * s:shapes)
+    {
+        s->draw(painter);
+    }
+
+}
+
+void MainWindow::on_b_draw_circle_clicked()
+{
+	pixmap->fill();
+
+	draw_circle(painter);
+
+	ui->label_draw_area->setPixmap(*pixmap);
+}
+
+void MainWindow::draw_square(QPainter * painter)
+{
+    shapes.push_back(new Square("yellow",rand() % 100,rand() % 500,rand() % 400));
+    for(Figure * s:shapes)
+        s->draw(painter);
+}
+void MainWindow::on_b_draw_square_clicked()
+{
+    pixmap->fill();
+
+    draw_square(painter);
+
+    ui->label_draw_area->setPixmap(*pixmap);
+}
+
+void MainWindow::draw_triangle(QPainter * painter)
+{
+    shapes.push_back(new Triangle(rand() % 100,rand() % 500,rand() % 400));
+    for(Figure * s:shapes)
+        s->draw(painter);
+}
+
+void MainWindow::on_b_draw_triangle_clicked()
+{
+    pixmap->fill();
+
+    draw_triangle(painter);
+
+    ui->label_draw_area->setPixmap(*pixmap);
+}
